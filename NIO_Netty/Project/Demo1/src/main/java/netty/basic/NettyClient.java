bc24954cf4af3c7d8f702e6f852871ad87c272ff
邓元俊 <598779014@qq.com>
2019-08-20 17:32:26 +0800

    update NIO

---
+++ NIO_Netty/Project/Demo1/src/main/java/netty/basic/NettyClient.java
@@ -0,0 +1,33 @@
+package netty.basic;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        //1. 创建一个线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        //2. 创建客户端的启动助手
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)  //3. 设置线程组
+                .channel(NioSocketChannel.class)    //4. 设置客户端通道的实现类
+                .handler(new ChannelInitializer<SocketChannel>() {  //5. 创建一个通道初始化对象
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new NettyClientHandler());
+                    }
+                });
+        System.out.println("......Client is ready......");
+        //7. 启动客户端去连接服务器端
+        ChannelFuture cf = bootstrap.connect("127.0.0.1",9999).sync();
+
+        //8. 关闭通道
+        cf.channel().closeFuture().sync();
+
+    }
+}
---
+++ NIO_Netty/Project/Demo1/src/main/java/netty/basic/NettyClientHandler.java
@@ -0,0 +1,24 @@
+package netty.basic;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+//客户端业务处理类
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Client测试，ctx:" + ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("老板还钱", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        System.out.println("服务器端发来的消息：" + buf.toString(CharsetUtil.UTF_8));
+    }
+
+
+}
